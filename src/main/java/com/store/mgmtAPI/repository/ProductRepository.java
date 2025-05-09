b5e0bab53dd80be1765fb0cb1a3e74d6b93294f1
Silvia <smaria497@gmail.com>
2025-05-09 23:12:58 +0300

    chore: initialize project with product repository (not adding findAll, findById, save and deleteById as are automatically provided)

--- src/main/java/com/store/mgmtAPI/repository/ProductRepository.java
+++ src/main/java/com/store/mgmtAPI/repository/ProductRepository.java
@@ -1,4 +1,31 @@
 package com.store.mgmtAPI.repository;
 
-public interface ProductRepository {
+import com.store.mgmtAPI.repository.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+
+    @Query("select prod from Product prod where name like %:productName%")
+    List<Product> findProductByName(@Param("productName") String productName);
+
+    @Query("select prod from Product prod where description like %:productDescription%")
+    List<Product> findProductByDescription(@Param("productDescription") String productDescription);
+
+    @Transactional
+    void deleteByName(String name);
+
+    @Transactional
+    @Modifying
+    @Query("update Product prod set prod.description= ?2, prod.price = ?3, prod.quantity = ?4 " +
+            "where prod.name = ?1")
+    void updateProduct(String name, String description, BigDecimal price, int quantity);
 }
