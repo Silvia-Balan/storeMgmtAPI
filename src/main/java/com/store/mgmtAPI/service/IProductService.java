eb215175d4572abc0da59f12b28a152bb6dc0f5b
Silvia <smaria497@gmail.com>
2025-05-09 23:27:43 +0300

    chore: add interface IProductService (defining what the service does)

--- src/main/java/com/store/mgmtAPI/service/IProductService.java
+++ src/main/java/com/store/mgmtAPI/service/IProductService.java
@@ -1,4 +1,21 @@
 package com.store.mgmtAPI.service;
 
+import com.store.mgmtAPI.repository.model.Product;
+import com.store.mgmtAPI.service.dto.ProductDTO;
+
+import java.util.List;
+
 public interface IProductService {
+
+    ProductDTO addProduct(Product product);
+
+    public List<ProductDTO> getAllProducts();
+    public ProductDTO getProduct(String name);
+    public List<ProductDTO> getAllProductsByName(String name);
+    public List<ProductDTO> getAllProductsByDescription(String description);
+
+    boolean deleteProduct(int id);
+    boolean deleteAllProducts();
+
+    public boolean updateProduct(int id, Product product);
 }
