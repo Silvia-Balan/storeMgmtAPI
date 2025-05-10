a33f991029e79e2e6077e65e9f85236e24021178
Silvia <smaria497@gmail.com>
2025-05-10 11:58:45 +0300

    chore: - add implementation ProductServiceImpl (defining how the actions from interface IProductService are done) - add classes for exception handling (GlobaExceptionHandler - for handling exceptions that appear in the api centrally; DetailedErrorResponse - for getting more detail to the client by using Http response body; ProductNameAlreadyExistsException - for handling the case when a name with an already existing value exists in the DB)

--- src/main/java/com/store/mgmtAPI/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/store/mgmtAPI/controller/exception/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.store.mgmtAPI.controller.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNameAlreadyExistsException.class)
+    public ResponseEntity<DetailedErrorResponse> handleNameAlreadyExists(ProductNameAlreadyExistsException exception, WebRequest webRequest){
+        DetailedErrorResponse detailedErrorResponse = new DetailedErrorResponse(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage()
+        );
+
+        return new ResponseEntity<>(detailedErrorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
