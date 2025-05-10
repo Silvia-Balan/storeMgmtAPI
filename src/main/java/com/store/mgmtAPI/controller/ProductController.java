761f3c70519b6a15703680ef214aa26645a16621
Silvia <smaria497@gmail.com>
2025-05-10 12:35:03 +0300

    chore: - add implementation ProductServiceImpl   -> add getAllProducts implementation   -> add logging - add ProductController for handling http requests related to products (for connecting to the API client)

--- src/main/java/com/store/mgmtAPI/controller/ProductController.java
+++ src/main/java/com/store/mgmtAPI/controller/ProductController.java
@@ -1,4 +1,44 @@
 package com.store.mgmtAPI.controller;
 
+import com.store.mgmtAPI.repository.model.Product;
+import com.store.mgmtAPI.service.IProductService;
+import com.store.mgmtAPI.service.dto.ProductDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/products", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductController {
+    private IProductService iProductService;
+
+    public ProductController(IProductService iProductService) {
+        this.iProductService = iProductService;
+    }
+
+    @GetMapping
+    public List<ProductDTO> getAllProducts(){
+        return iProductService.getAllProducts();
+    }
+    @PostMapping
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product, UriComponentsBuilder uriComponentsBuilder){
+        System.out.println("Product is: " + product);
+        ProductDTO productDTO = iProductService.addProduct(product);
+
+        URI locationURI = uriComponentsBuilder
+                .path("/products/" + productDTO.getName())
+                .buildAndExpand(uriComponentsBuilder.toUriString())
+                .toUri();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .location(locationURI)
+                .body(productDTO);
+    }
+
+
+
 }
--- src/main/java/com/store/mgmtAPI/service/impl/ProductServiceImpl.java
+++ src/main/java/com/store/mgmtAPI/service/impl/ProductServiceImpl.java
@@ -1,4 +1,88 @@
 package com.store.mgmtAPI.service.impl;
 
-public class ProductServiceImpl {
+import com.store.mgmtAPI.controller.exception.ProductNameAlreadyExistsException;
+import com.store.mgmtAPI.repository.ProductRepository;
+import com.store.mgmtAPI.repository.model.Product;
+import com.store.mgmtAPI.service.IProductService;
+import com.store.mgmtAPI.service.dto.ProductDTO;
+import com.store.mgmtAPI.service.dto.ProductMapper;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class ProductServiceImpl implements IProductService {
+    private ProductRepository productRepository;
+    private static final Logger log = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ProductDTO addProduct(Product product) {
+        log.info("Attempting to add new product: {}", product.getName());
+
+        if(productRepository.findByName(product.getName()).isPresent()){
+            // product name already exists in DB, not adding it again
+            log.error("Failed to add product: {}", product.getName());
+            throw  new ProductNameAlreadyExistsException("The product " + product.getName() + " already exists in DB, choose another name and try again ");
+        }
+
+        Product savedProd = productRepository.save(product);
+        log.info("Successfully added product with ID: {}", savedProd.getId());
+        ProductDTO productDTO = ProductMapper.mapToProductDTO(savedProd, new ProductDTO());
+
+        return productDTO;
+    }
+
+
+    @Override
+    public List<ProductDTO> getAllProducts() {
+
+        log.info("Attempting to retrieve a list of all products...");
+
+        List<ProductDTO> listOfProductDTO = new ArrayList<>();
+
+        for(Product product : productRepository.findAll()){
+            ProductDTO productDTO = ProductMapper.mapToProductDTO(product, new ProductDTO());
+            listOfProductDTO.add(productDTO);
+        }
+        log.info("List of products retrieved successfully.");
+        return listOfProductDTO;
+    }
+
+    @Override
+    public ProductDTO getProduct(String name) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDTO> getAllProductsByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDTO> getAllProductsByDescription(String description) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteProduct(int id) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteAllProducts() {
+        return false;
+    }
+
+    @Override
+    public boolean updateProduct(int id, Product product) {
+        return false;
+    }
 }
