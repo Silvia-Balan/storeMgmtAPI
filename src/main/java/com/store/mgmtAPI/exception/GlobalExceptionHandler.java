ed5d4d03bdb0deda0e007658e42f000a3a7b201d
Silvia <smaria497@gmail.com>
2025-05-11 17:01:13 +0300

    feature: add-validation-annotations - add dependency for validation - add field length restriction - add validation for product

--- src/main/java/com/store/mgmtAPI/exception/GlobalExceptionHandler.java
+++ src/main/java/com/store/mgmtAPI/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.store.mgmtAPI.exception;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import org.slf4j.Logger;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(ProductNameAlreadyExistsException.class)
     public ResponseEntity<DetailedErrorResponse> handleNameAlreadyExists(ProductNameAlreadyExistsException exception, WebRequest webRequest){
         DetailedErrorResponse detailedErrorResponse = new DetailedErrorResponse(
@@ -40,5 +46,18 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(detailedErrorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex){
+        BindingResult bindingResult = ex.getBindingResult();
+        String errorMessage = bindingResult.getAllErrors()
+                .stream()
+                .map(errObj -> errObj.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        logger.error("Validation errors: {}", errorMessage);  // Log validation error message
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ src/main/java/com/store/mgmtAPI/utils/FieldLengths.java
@@ -0,0 +1,7 @@
+package com.store.mgmtAPI.utils;
+
+public class FieldLengths {
+    public static final int PRODUCT_NAME_MAX_LENGTH = 100;
+    public static final int PRODUCT_NAME_MIN_LENGTH = 3;
+    public static final int DESCRIPTION_MAX_LENGTH = 500;
+}
